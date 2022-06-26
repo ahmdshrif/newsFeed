78fd22e202c85cbff2d5bf591df5b550236bc75d
Ahmed Sherif <Ahmed.Sherif1@vodafone.com>
2022-06-26 15:04:21 +0200

    feat : support deepLinking

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,8 @@
  */
 
 import React from 'react';
-// import {useColorScheme} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {useColorScheme} from 'react-native';
+import {NavigationContainer, getStateFromPath} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {MainScreen, DetailScreen, SettingsScreen} from './screens';
@@ -18,6 +18,7 @@ import {useTranslation} from 'react-i18next';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+import {DefaultTheme, DarkTheme} from '@react-navigation/native';
 
 const MainStackNavigation = () => {
   const {t} = useTranslation();
@@ -40,9 +41,54 @@ const MainStackNavigation = () => {
 
 const App = () => {
   const {t} = useTranslation();
+  const scheme = useColorScheme();
+  const linking = {
+    prefixes: ['newsFeed://'],
+    config: {
+      initialRouteName: 'Settings',
+      screens: {
+        News: {
+          path: 'news',
+          screens: {
+            Home: {
+              path: 'home',
+            },
+            Details: {
+              path: 'details/:title/:content/:urlToImage',
+            },
+          },
+        },
+        Settings: {
+          path: 'settings',
+        },
+      },
+    },
+    getStateFromPath: (path: any, options: any) => {
+      const state = getStateFromPath(path, options);
+      console.log(state);
 
+      const newState = {
+        ...state,
+        routes: state?.routes.map(route => {
+          if (route.name === 'details') {
+            console.log('details route');
+            // modify your params however you like here!
+            return {
+              ...route,
+              params: {data: route.params},
+            };
+          } else {
+            return route;
+          }
+        }),
+      };
+      return newState;
+    },
+  };
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      linking={linking}
+      theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Tab.Navigator>
         <Tab.Screen
           options={{
--- src/types.tsx
+++ src/types.tsx
@@ -1,12 +1,12 @@
 export interface newsFeedInterface {
-  source: {
+  source?: {
     id?: string | null;
     name?: string;
   };
   title: string;
-  description: string;
   content: string;
-  url: string;
+  description?: string;
+  url?: string;
   author?: string | null;
   urlToImage?: string | null;
   publishedAt?: string;
